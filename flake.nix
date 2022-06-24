0c9593f81f936a7c2f555ad7f840edf06bdcb98b
Carlos Hernandez <chernand@yelp.com>
2022-06-23 22:29:21 -0600

    Cleanup cruft

--- flake.nix
+++ flake.nix
@@ -6,33 +6,25 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    let
-      eachDefaultSystemMap = utils.lib.eachSystemMap utils.lib.defaultSystems;
-    in {
-      packages = eachDefaultSystemMap ( system: rec {
-        snapraid-runner =
-          with import nixpkgs { inherit system; };
-          with pkgs.python3Packages;
-          buildPythonApplication {
-            pname = "snapraid-runner";
-            version = "1.0";
+  outputs = { self, nixpkgs, utils }: {
+    packages = utils.lib.eachDefaultSystemMap ( system: rec {
+      snapraid-runner =
+        with import nixpkgs { inherit system; };
+        with pkgs.python3Packages;
+        buildPythonApplication {
+          pname = "snapraid-runner";
+          version = "1.0";
 
-            propagatedBuildInputs = [ apprise ];
+          propagatedBuildInputs = [ apprise ];
 
-            src = ./.;
-          };
-        default = snapraid-runner;
-      } );
+          src = ./.;
+        };
+      default = snapraid-runner;
+    } );
 
-      overlays.snapraid-runner = final: prev: {
-        inherit (self.packages.${final.system}) snapraid-runner;
-      };
-      overlays.default = self.overlays.snapraid-runner;
-
-      nixosModules.snapraid-runner = { ... }: {
-        nixpkgs.overlays = self.overlays.snapraid-runner;
-      };
-      nixosModules.default = self.nixosModules.snapraid-runner;
+    overlays.snapraid-runner = final: prev: {
+      inherit (self.packages.${final.system}) snapraid-runner;
     };
+    overlays.default = self.overlays.snapraid-runner;
+  };
 }
